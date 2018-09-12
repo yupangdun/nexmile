d7aa19f3aa739f85fa91749ecd06592ec670f339
zl.yu <zl.yu@gtscloud.cc>
2018-09-12 22:38:15 +0800

    添加轮播

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,11 +14,11 @@
         </div>
       </div>
     </div>
-    <el-carousel arrow="never" trigger="click" indicator-position="none">
+    <el-carousel arrow="never" trigger="click">
       <el-carousel-item v-for="(item,index) in bannerList" indicator-position="none" :key="index" style="text-align: center;">
-        <img style="width: 100%;height: 100%;" :src="header.src" />
-        <p class="p1">{{header.h1}}</p>
-        <p class="p2">{{header.h2}}</p>
+        <img style="width: 100%;height: 100%;" :src="item.src" />
+        <p class="p1">{{item.h1}}</p>
+        <p class="p2">{{item.h2}}</p>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -52,7 +52,33 @@ export default {
   data () {
     return {
       icon: require('../assets/logo.png'),
-      bannerList: [{}]
+      bannerList: [
+        {
+          src: require('../assets/banner/banner3.jpg'),
+          h1: 'OUR VISION',
+          h2: '成为全球最佳的功能材料解决方案开拓者以及提供商'
+        },
+        {
+          src: require('../assets/banner/banner1.jpg'),
+          h1: '产品解决方案',
+          h2: '电磁屏蔽&兼容，射频吸收，无线充电解决方案'
+        },
+        {
+          src: require('../assets/banner/banner4.jpg'),
+          h1: '应用案例',
+          h2: '广泛应用于通讯设备、航空航天、医疗、电子、机械制造、轨道交通、新能源等行业'
+        },
+        {
+          src: require('../assets/banner/banner5.jpg'),
+          h1: '资讯',
+          h2: '在功能材料领域为客户探求更适的资源和创造更佳的解决方案'
+        },
+        {
+          src: require('../assets/banner/banner2.jpg'),
+          h1: '关于NEXMILE',
+          h2: '深入挖掘功能材料行业特点，以合理的价格，优质的产品，快速的响应，优质的供应链管理，提供给客户全新非凡的价值体验'
+        }
+      ]
     }
   },
   computed: {
